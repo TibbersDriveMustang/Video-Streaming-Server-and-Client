4e75439fa4901617c5942c9545f82a45ab76fcdc
Hongyi Guo <guohongyi1991@gmail.com>
2015-11-20 16:56:36 -0600

    frameLength be assigned directly by "15" ,still can`t translate to integer

--- VideoStream.py
+++ VideoStream.py
@@ -4,6 +4,7 @@ class VideoStream:
 		self.filename = filename
 		try:
 			self.file = open(filename, 'rb')
+			print filename + " read in VideoStream.py"
 		except:
 			print "read " + filename + " error"
 			raise IOError
@@ -12,10 +13,10 @@ class VideoStream:
 	def nextFrame(self):
 		"""Get next frame."""
 		data = self.file.read(5) # Get the framelength from the first 5 bits
-
+		print "framelength is " + data
 		if data:
-			framelength = int(data,16)
-			print "framelength is " + framelength
+
+			framelength = 15 #int(data,16)
 			# Read the current frame
 			data = self.file.read(framelength)
 			self.frameNum += 1
